e3cb6092b4d7063e38f98b6f22cc6a4c46fd861d
Kauã Henrique <kaua.ribeiro@ufu.br>
2026-06-29 00:03:18 -0300

    fix: adjust base path case for github pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/ufudrive/',
+  base: '/UFUDrive/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
